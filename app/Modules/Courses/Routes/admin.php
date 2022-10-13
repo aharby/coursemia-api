a72d4093df4805c684583e918f7c2764fd5038e4
Ramy Mohamed <ramy.elsabahy@4sale.tech>
2022-10-13 23:28:03 +0200

    Editing course

--- app/Modules/Courses/Routes/admin.php
+++ app/Modules/Courses/Routes/admin.php
@@ -5,4 +5,5 @@ use App\Modules\Courses\Controllers\Admin\CoursesAdminController;
 Route::group(['prefix' => 'courses', 'as' => 'courses.'], function () {
     Route::get('/', [CoursesAdminController::class, 'index']);
     Route::get('/{id}', [CoursesAdminController::class , 'show']);
+    Route::put('/{id}', [CoursesAdminController::class , 'update']);
 });
