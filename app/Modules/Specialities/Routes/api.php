d57e0f71d0de6b4b5e91928f9aac8f8d3ec93ebf
Ramy Mohamed <ramy.elsabahy@4sale.tech>
2022-10-14 19:36:15 +0200

    Adding max allowed devices when login

--- app/Modules/Specialities/Routes/api.php
+++ app/Modules/Specialities/Routes/api.php
@@ -2,5 +2,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'specialities', 'as' => 'specialities.', 'namespace' => '\App\Modules\Specialities\Controllers\API'], function () {
-    Route::get('specialities', 'SpecialitiesApiControllers@index');
+    Route::get('/', 'SpecialitiesApiControllers@index');
 });
