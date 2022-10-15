1b8c32073e046fe4f30e1d56f31ca5e17492c42f
Ramy Mohamed <ramy.elsabahy@4sale.tech>
2022-10-15 23:57:24 +0200

    Adding specialities

---
+++ app/Modules/Courses/Resources/API/SpecialitiesResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Modules\Courses\Resources\API;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SpecialitiesResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'            => $this->id,
+            'title'        => $this->translated_title,
+        ];
+    }
+}
--- app/Modules/HomeScreen/Controllers/HomeScreenController.php
+++ app/Modules/HomeScreen/Controllers/HomeScreenController.php
@@ -8,10 +8,12 @@ use App\Modules\Courses\Models\Course;
 use App\Modules\Courses\Models\Question;
 use App\Modules\Courses\Resources\API\CoursesResource;
 use App\Modules\Courses\Resources\API\QuestionResource;
+use App\Modules\Courses\Resources\API\SpecialitiesResource;
 use App\Modules\Events\Models\Event;
 use App\Modules\Events\Resources\API\EventsResource;
 use App\Modules\Offers\Models\Offer;
 use App\Modules\Offers\Resources\API\OffersResource;
+use App\Modules\Specialities\Models\Speciality;
 
 class HomeScreenController extends Controller
 {
@@ -20,12 +22,13 @@ class HomeScreenController extends Controller
         $events = Event::get();
         $question = Question::inRandomOrder()->first();
         $courses = Course::take(5)->get();
+        $specialities = Speciality::get();
         return customResponse([
             "offers" => OffersResource::collection($offers),
             "events" => EventsResource::collection($events),
             "question_of_the_day"   => new QuestionResource($question),
             "specialities"          => [
-                // @todo to be fetched from DB after karim finishes
+                SpecialitiesResource::collection($specialities),
             ],
             "courses"   => CoursesResource::collection($courses)
         ], __("Get home content successfully"), 200, StatusCodesEnum::DONE);
