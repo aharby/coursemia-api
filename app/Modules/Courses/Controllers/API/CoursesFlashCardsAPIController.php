c5bdd367a5c418092d56ac7b193d830db573b763
Ramy Mohamed <ramy.elsabahy@4sale.tech>
2022-10-16 11:49:23 +0200

    Adding flashcards and changing things in questions response

---
+++ app/Modules/Courses/Controllers/API/CoursesFlashCardsAPIController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Modules\Courses\Controllers\API;
+
+use App\Enums\StatusCodesEnum;
+use App\Http\Controllers\Controller;
+use App\Modules\Countries\Resources\Api\ListCountriesIndexPaginator;
+use App\Modules\Courses\Models\Course;
+use App\Modules\Courses\Models\CourseFlashcard;
+use App\Modules\Courses\Models\CourseLecture;
+use App\Modules\Courses\Models\CourseNote;
+use App\Modules\Courses\Repository\QuestionsRepositoryInterface;
+use App\Modules\Courses\Resources\API\CourseDetailsResource;
+use App\Modules\Courses\Resources\API\CourseLectureResource;
+use App\Modules\Courses\Resources\API\CourseNoteResource;
+use App\Modules\Courses\Resources\API\CoursesCollection;
+use App\Modules\Courses\Resources\API\CoursesResource;
+use App\Modules\Courses\Resources\API\FlashCardsResource;
+use App\Modules\Courses\Resources\Api\ListCourseQuestionsPaginator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CoursesFlashCardsAPIController extends Controller
+{
+    public function getCourseFlashCards($courseId)
+    {
+        $category_id = \request()->category_id;
+        $flashs = CourseFlashcard::query();
+        $flashs->where('course_id', $courseId);
+        if (isset($category_id)){
+            $flashs->where('category_id', $category_id);
+        }
+        $flash_cards = $flashs->get();
+        return customResponse(FlashCardsResource::collection($flash_cards), trans('api.course flashcards'), 200, StatusCodesEnum::DONE);
+    }
+}
---
+++ app/Modules/Courses/Resources/API/FlashCardsResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Modules\Courses\Resources\API;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FlashCardsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'            => $this->id,
+            'front'         => $this->front,
+            'back'          => $this->back,
+            'answer'        => (boolean)$this->answer
+        ];
+    }
+}
