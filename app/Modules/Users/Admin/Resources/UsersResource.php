4e5c78c3124947743fc53d03fe5bc683bdc9c685
Ramy Mohamed <ramy.elsabahy@4sale.tech>
2022-10-27 12:21:19 +0200

    added all course related things in create

--- app/Modules/Users/Admin/Resources/UsersResource.php
+++ app/Modules/Users/Admin/Resources/UsersResource.php
@@ -4,6 +4,7 @@ namespace App\Modules\Users\Admin\Resources;
 
 use App\Modules\Courses\Models\Category;
 use App\Modules\Courses\Models\CourseLecture;
+use App\Modules\Courses\Resources\Admin\CoursesResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\App;
 
@@ -24,7 +25,7 @@ class UsersResource extends JsonResource
             'email'         => $this->email,
             'phone'         => $this->country_code.$this->phone,
             'referral_code' => $this->refer_code,
-            'courses_bought'=> $this->courses,
+            'courses_bought'=> CoursesResource::collection($this->courses),
             'avatar'        => asset($this->photo),
             'role'          => 'editor',
             'is_active'     => (bool)$this->is_active,
