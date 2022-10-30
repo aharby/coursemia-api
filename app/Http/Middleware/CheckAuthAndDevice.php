a74e4fe8666ec1042cd245ce8a5d13f4cbc84b9c
Ramy Mohamed <ramy.elsabahy@4sale.tech>
2022-10-30 11:27:56 +0200

    Adding middleware to check user suspended

--- app/Http/Middleware/CheckAuthAndDevice.php
+++ app/Http/Middleware/CheckAuthAndDevice.php
@@ -25,7 +25,7 @@ class CheckAuthAndDevice
                 ->orWhere('device_id', $device_id);
         })->first();
         if (isset($token) && !isset($device)){
-            return customResponse(null,__("Device was logged out"), 422, StatusCodesEnum::FAILED);
+            return customResponse(null,__("Device was logged out"), 401, StatusCodesEnum::FAILED);
         }
         return $next($request);
     }
