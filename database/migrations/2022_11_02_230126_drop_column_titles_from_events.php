eddc5daa4a7525f30e9e4eccbb51295c2802826c
Ramy Mohamed <ramy.elsabahy@4sale.tech>
2022-11-02 22:02:39 +0200

    Added lectures and events

---
+++ database/migrations/2022_11_02_230126_drop_column_titles_from_events.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropColumnTitlesFromEvents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            if (Schema::hasColumn('events', 'title_en'))
+            {
+                Schema::table('events', function (Blueprint $table)
+                {
+                    $table->dropColumn('title_en');
+                });
+            }
+            if (Schema::hasColumn('events', 'title_ar'))
+            {
+                Schema::table('events', function (Blueprint $table)
+                {
+                    $table->dropColumn('title_ar');
+                });
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            //
+        });
+    }
+}
