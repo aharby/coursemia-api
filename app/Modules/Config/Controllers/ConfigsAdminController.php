5520fb54382747cda02adb8f38ca7ad05f966843
karimabdelaah <karim.abdullah@our-edu.net>
2022-11-12 17:09:07 +0200

    add about us feat

---
+++ app/Modules/Config/Controllers/ConfigsAdminController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Modules\Config\Controllers;
+
+use App\Enums\StatusCodesEnum;
+use App\Http\Controllers\Controller;
+use App\Modules\BaseApp\Enums\ParentEnum;
+use App\Modules\Config\Config;
+use App\Modules\Config\Repository\ConfigRepositoryInterface;
+use App\Modules\Config\Requests\ConfigRequest;
+use App\Modules\Countries\Resources\Api\ListAdminConfigsIndex;
+use App\Modules\Users\Admin\Middleware\IsSuperAdmin;
+use App\VersionConfig;
+use Intervention\Image\Facades\Image;
+
+class ConfigsAdminController extends Controller
+{
+
+
+    public function __construct(public ConfigRepositoryInterface $configRepository)
+    {
+    }
+
+    public function show()
+    {
+        $configs = $this->configRepository->get();
+        return customResponse(ListAdminConfigsIndex::collection($configs), __('Done'), 200, StatusCodesEnum::DONE);
+    }
+
+    public function update(ConfigRequest $request)
+    {
+        $rows = $request->all();
+        if ($rows) {
+            foreach ($rows as $row) {
+                $rowToUpdate = Config::find($row['id']);
+                if ($rowToUpdate) {
+                    $rowToUpdate->update([
+                        'value:en' => $row['value_en'],
+                        'value:ar' => $row['value_ar'],
+                    ]);
+                }
+            }
+        }
+        \Cache::forget('configs');
+        updateConfigsCache();
+        return customResponse('', trans('api.Updated Successfully'), 200, 1);
+    }
+
+}
