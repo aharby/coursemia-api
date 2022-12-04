f92c195acb91ad0f0051d87c360e5f9d41d03bcd
Ramy Mohamed <ramy.elsabahy@4sale.tech>
2022-12-04 18:17:52 +0200

    Solving sheet issues

--- app/Modules/Category/Resources/API/SubCategoriesResource.php
+++ app/Modules/Category/Resources/API/SubCategoriesResource.php
@@ -7,7 +7,7 @@ use App\Modules\Courses\Models\CourseLecture;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\App;
 
-class CategoriesResource extends JsonResource
+class SubCategoriesResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -21,7 +21,6 @@ class CategoriesResource extends JsonResource
         return [
             'id'            => $this->id,
             'title'         => $this->title,
-            'subs'          => SubCategoriesResource::collection($this->subs),
             'have_free_content' => $lecs ? true : false
         ];
     }
