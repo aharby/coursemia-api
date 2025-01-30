9c37645d005db7408198a988fdeeecf70128192a
Abanoub <abanoubharby@gmail.com>
2025-01-30 12:17:32 +0100

    add files

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    // LoginUseCase
+    'Invalid login details' => 'Invalid login details',
+    'Welcome to your dashboard' => 'Welcome to your dashboard',
+    'This account is banned' => 'This account is banned',
+    'Logged in successfully' => 'Logged in successfully',
+    'User not verified' => 'User not verified',
+    'This account is not confirmed' => 'This account is not confirmed',
+    'Maximum device numbers exceeded' => 'Maximum device numbers exceeded',
+    'This account is suspended' => 'This account is suspended',
+    'There is no account with this email' => 'There is no account with this email',
+    'Oopps Something is broken' => 'Oopps Something is broken',
+    'Trying to login with invalid password' => 'Trying to login with invalid password',
+    'Trying to login with non super admin account' => 'Trying to login with non super admin account',
+    'Cannot Login From This Device' => 'Cannot Login From This Device',
+
+    // AuthApiController
+    'Device not found' => 'Device not found',
+    'Phone number verified successfully' => 'Phone number verified successfully',
+    'verification failed' => 'verification failed',
+    'Language changed successfully' => 'Language changed successfully',
+    'Account activated' => 'Account activated',
+    'Devices deleted successfully' => 'Devices deleted successfully',
+    'User does not exist.' => 'User does not exist.',
+    'Token stored successfully' => 'Token stored successfully',
+    'invalid code' => 'invalid code',
+    'Email already taken.' => 'Email already taken.',
+    'Maybe the token expired or malformed' => 'Maybe the token expired or malformed',
+    'Push notifications enabled' => 'Push notifications enabled',
+    'Can not delete your current device' => 'Can not delete your current device',
+    'Device deleted successfully' => 'Device deleted successfully',
+    'Push notifications disabled' => 'Push notifications disabled',
+    'Profile updated successfully' => 'Profile updated successfully',
+];
