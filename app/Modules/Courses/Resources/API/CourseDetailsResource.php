2763509d493de61a303eec302255afd793fb8745
Abanoub <abanoubharby@gmail.com>
2025-07-30 22:29:15 +0200

    keep categroy order on course details

--- app/Modules/Courses/Resources/API/CourseDetailsResource.php
+++ app/Modules/Courses/Resources/API/CourseDetailsResource.php
@@ -29,11 +29,12 @@ class CourseDetailsResource extends JsonResource
         $is_in_cart = false;
 
         $user = auth('api')->user();
-        if (isset($user)){
-            $user_id = auth('api')->user()->id;
+        if (isset($user)) {
+            $user_id = $user->id;
             $course_user = CourseUser::where(['course_id' => $this->id, 'user_id' => $user_id])->first();
-            if (isset($course_user))
+            if (isset($course_user)) {
                 $is_purchased = true;
+            }
 
             $is_in_cart = $user->cartCourses()->where(['course_id' => $this->id])->exists();
         }
@@ -42,45 +43,58 @@ class CourseDetailsResource extends JsonResource
         $notes = $this->notes();
         $questions = $this->questions();
         $flashcards = $this->flashCards();
-        $lectures_categories = $lectures->pluck('category_id')->unique();
-        $notes_categories = $notes->pluck('category_id')->unique();
-        $questions_categories = $questions->pluck('category_id')->unique();
-        $flashcards_categories = $flashcards->pluck('category_id')->unique();
+
+        $lectures_categories_ids = $lectures->pluck('category_id')->unique()->filter();
+        $notes_categories_ids = $notes->pluck('category_id')->unique()->filter();
+        $questions_categories_ids = $questions->pluck('category_id')->unique()->filter();
+        $flashcards_categories_ids = $flashcards->pluck('category_id')->unique()->filter();
+
         $images = $this->images()->pluck('image');
-        $offer_courses_check = OfferCourse::where('course_id', $this->id)->first();
-        $price_after_discount = null;
-//        if (isset($offer_courses_check)){
-//            $value = $offer_courses_check->offer->offer_value;
-//            $price_after_discount = $this->price - (($value*$this->price) / 100);
-//        }
+
         $price_after_discount = $this->price_after_discount;
-        if (isset($this->expire_date)){
-            $expire = Carbon::parse($this->expire_date)->format('Y-m-d');
-        }else{
-            $expire = $this->expire_duration;
-        }
+
+        $expire = isset($this->expire_date)
+            ? Carbon::parse($this->expire_date)->format('Y-m-d')
+            : $this->expire_duration;
+
+        // Preserve category order
+        $lecture_cats = Category::whereIn('id', $lectures_categories_ids)->get()->keyBy('id');
+        $ordered_lecture_categories = $lectures_categories_ids->map(fn($id) => $lecture_cats[$id])->filter();
+
+        $notes_cats = Category::whereIn('id', $notes_categories_ids)->get()->keyBy('id');
+        $ordered_notes_categories = $notes_categories_ids->map(fn($id) => $notes_cats[$id])->filter();
+
+        $questions_cats = Category::whereIn('id', $questions_categories_ids)->get()->keyBy('id');
+        $ordered_questions_categories = $questions_categories_ids->map(fn($id) => $questions_cats[$id])->filter();
+
+        $flashcards_cats = Category::whereIn('id', $flashcards_categories_ids)->get()->keyBy('id');
+        $ordered_flashcards_categories = $flashcards_categories_ids->map(fn($id) => $flashcards_cats[$id])->filter();
+
         return [
-            'id'            => $this->id,
-            'is_in_my_cart' => false, //@todo implement is in my cart
-            'is_purchased'  => $is_purchased,
-            'is_in_cart'    => $is_in_cart,
-            'title'         => $this->title,
-            'cover_image'   => asset($this->cover_image),
-            'images'        => $images,
-            'price'         => (double)$this->price,
-            'price_after_discount'         => (double)$price_after_discount,
-            'rate'          => (float)$this->rate,
-            'description'   => $this->description,
+            'id' => $this->id,
+            'is_in_my_cart' => false, // @todo implement is in my cart
+            'is_purchased' => $is_purchased,
+            'is_in_cart' => $is_in_cart,
+            'title' => $this->title,
+            'cover_image' => asset($this->cover_image),
+            'images' => $images,
+            'price' => (double) $this->price,
+            'price_after_discount' => (double) $price_after_discount,
+            'rate' => (float) $this->rate,
+            'description' => $this->description,
             'reviews_count' => $this->reviews()->count(),
             'expiration_date' => $expire,
-            'lectures_count'=> $lectures->count(),
-            'notes_count'   => $notes->count(),
-            'questions_count'=> $questions->count(),
-            'flash_cards_count'=> $flashcards->count(),
-            'lectures_categories' => LectureCategoriesResource::collection(Category::whereIn('id', $lectures_categories)->groupBy('id','parent_id')->get()),
-            'notes_categories' => NotesCategoriesResource::collection(Category::whereIn('id', $notes_categories)->groupBy('id','parent_id')->get()),
-            'questions_categories' => QuestionsCategoriesResource::collection(Category::whereIn('id', $questions_categories)->groupBy('id','parent_id')->get()),
-            'flash_cards_categories' => FlashCardsCategoriesResource::collection(Category::whereIn('id', $flashcards_categories)->groupBy('id','parent_id')->get()),
+            'lectures_count' => $lectures->count(),
+            'notes_count' => $notes->count(),
+            'questions_count' => $questions->count(),
+            'flash_cards_count' => $flashcards->count(),
+
+            // Ordered categories preserved
+            'lectures_categories' => LectureCategoriesResource::collection($ordered_lecture_categories),
+            'notes_categories' => NotesCategoriesResource::collection($ordered_notes_categories),
+            'questions_categories' => QuestionsCategoriesResource::collection($ordered_questions_categories),
+            'flash_cards_categories' => FlashCardsCategoriesResource::collection($ordered_flashcards_categories),
         ];
     }
+
 }
